56eec1e2f6d236ff3e286aacd0fb24181b4c6799
Moon <191705595@qq.com>
2018-08-19 15:08:55 +0800

    更新seeders章节的eslint

--- chapter7/hapi-tutorial-2/seeders/20180819061006-init-shops.js
+++ chapter7/hapi-tutorial-2/seeders/20180819061006-init-shops.js
@@ -7,10 +7,18 @@ module.exports = {
   up: queryInterface => queryInterface.bulkInsert(
     'shops',
     [
-      { id: 1, name: '店铺1', thumb_url: '1.png', ...timestamps },
-      { id: 2, name: '店铺2', thumb_url: '2.png', ...timestamps },
-      { id: 3, name: '店铺3', thumb_url: '3.png', ...timestamps },
-      { id: 4, name: '店铺4', thumb_url: '4.png', ...timestamps },
+      {
+        id: 1, name: '店铺1', thumb_url: '1.png', ...timestamps,
+      },
+      {
+        id: 2, name: '店铺2', thumb_url: '2.png', ...timestamps,
+      },
+      {
+        id: 3, name: '店铺3', thumb_url: '3.png', ...timestamps,
+      },
+      {
+        id: 4, name: '店铺4', thumb_url: '4.png', ...timestamps,
+      },
     ],
     {},
   ),
