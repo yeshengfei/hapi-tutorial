6a2d491430821c60d839e95766d9339df99c7788
Moon <191705595@qq.com>
2018-08-23 23:26:33 +0800

    增加jwt解码的内容获取的测试

--- chapter10/hapi-tutorial-1/plugins/hapi-auth-jwt2.js
+++ chapter10/hapi-tutorial-1/plugins/hapi-auth-jwt2.js
@@ -2,15 +2,27 @@ const config = require('../config');
 
 const validate = (decoded, request, callback) => {
   let error;
+  /*
+    接口 POST /users/createJWT 中的 jwt 签发规则
+
+    const payload = {
+      userId: jwtInfo.userId,
+      exp: Math.floor(new Date().getTime() / 1000) + 7 * 24 * 60 * 60,
+    };
+    return JWT.sign(payload, process.env.JWT_SECRET);
+  */
+
   // decoded 为 JWT payload 被解码后的数据
   const userId = decoded.userId || {};
 
   if (!userId) {
     return callback(error, false, userId);
   }
-  return callback(error, true, {
+  const credentials = {
     userId,
-  });
+  };
+  // 在路由接口的 handler 通过 request.auth.credentials 获取 jwt decoded 的值
+  return callback(error, true, credentials);
 };
 
 module.exports = (server) => {
