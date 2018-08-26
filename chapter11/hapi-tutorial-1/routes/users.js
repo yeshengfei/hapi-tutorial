972b67a0beb9f113ecf947f03b17f83d060db821
moon <shengfei.ye@onebank.com.cn>
2018-08-27 01:44:25 +0800

    更新第11章小程序的jwt签发

--- chapter11/hapi-tutorial-1/routes/users.js
+++ chapter11/hapi-tutorial-1/routes/users.js
@@ -1,6 +1,9 @@
 const JWT = require('jsonwebtoken');
 const Joi = require('joi');
+const axios = require('axios');
 const config = require('../config');
+const models = require('../models');
+const decryptData = require('../utils/decryped-data');
 
 const GROUP_NAME = 'users';
 
@@ -30,7 +33,49 @@ module.exports = [
     method: 'POST',
     path: `/${GROUP_NAME}/wxLogin`,
     handler: async (req, reply) => {
-      reply();
+      const appid = config.wxAppid; // 你的小程序 appId
+      const secret = config.wxSecret; // 你的小程序 appSecret
+      const { code, encryptedData, iv } = req.payload;
+      // 向微信小程序开放平台 换取 openid 与 session_key
+      const response = await axios({
+        url: 'https://api.weixin.qq.com/sns/jscode2session',
+        method: 'GET',
+        params: {
+          appid,
+          secret,
+          js_code: code,
+          grant_type: 'authorization_code',
+        },
+      });
+      // response 中返回 openid 与 session_key
+      const { openid, session_key: sessionKey } = response.data;
+      // 基于 openid 查找或创建一个用户
+      const user = await models.users.findOrCreate({
+        where: { open_id: openid },
+      });
+      // decrypt 解码用户信息
+      const userInfo = decryptData(encryptedData, iv, sessionKey, appid);
+      // 更新user表中的用户的资料信息
+      await models.users.update({
+        nick_name: userInfo.nickName,
+        gender: userInfo.gender,
+        avatar_url: userInfo.avatarUrl,
+        open_id: openid,
+        session_key: sessionKey,
+      }, {
+        where: { open_id: openid },
+      });
+      // 签发 jwt
+      const generateJWT = (jwtInfo) => {
+        const payload = {
+          userId: jwtInfo.userId,
+          exp: Math.floor(new Date().getTime() / 1000) + 7 * 24 * 60 * 60,
+        };
+        return JWT.sign(payload, config.jwtSecret);
+      };
+      reply(generateJWT({
+        userId: user[0].id,
+      }));
     },
     config: {
       auth: false, // 不需要用户验证
